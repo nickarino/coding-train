f569bc7e86fa0180013e367431023229c17a6986
Nick Skriloff <skriloffn@darden.virginia.edu>
2021-12-27 20:13:04 -0500

    pendulum

--- CodingChallenges/CC_159_simple_pendulum_simulation/P5/Nick/sketch.js
+++ CodingChallenges/CC_159_simple_pendulum_simulation/P5/Nick/sketch.js
@@ -1,5 +1,10 @@
-// browser-sync start --server -f -w
-// cd Nick
+// browser-sync start --server -f -w ; cd Nick
+// https://www.myphysicslab.com/pendulum/pendulum-en.html?collection=col10279%2F1.33
+/**
+ * onenote
+ * https://ourdecisions-my.sharepoint.com/personal/nick_ourdecisions_com/_layouts/OneNote.aspx?id=%2Fpersonal%2Fnick_ourdecisions_com%2FDocuments%2FNotebooks%2FNickSkriloffWorld&wd=target%28MathAndScience%2FMath.one%7C69226B9C-0E35-47F3-AB9A-D5CD0B5E05E6%2FThe%20Coding%20Train%7C859B5548-888B-654A-B6B5-4C364E57BB56%2F%29
+onenote:https://ourdecisions-my.sharepoint.com/personal/nick_ourdecisions_com/Documents/Notebooks/NickSkriloffWorld/MathAndScience/Math.one#The%20Coding%20Train&section-id={69226B9C-0E35-47F3-AB9A-D5CD0B5E05E6}&page-id={859B5548-888B-654A-B6B5-4C364E57BB56}&object-id={4BF645B8-CF8D-8F4B-8758-A5DA0001A52A}&1C
+ */
 
 /// <reference path="global.d.ts" />
 
@@ -41,11 +46,13 @@ function setup() {
     //The longer the arm length the less the angle needs to change to travel a certain distance
     let forceOfPendulum = gravity * sin(angleFromYaxisToPendulumLine); 
     angleFromYaxisToPendulumLineAcceleration = (-1 * forceOfPendulum)/pendulumLen; //M = 1. so, Acceleration = Force
+    // The particular force of gravity is some constant (9.8M/s^2).  In this case we are going to make gravity one. 
     //update the velocity based on the acceleration with its rate of change. 
+    //Why because whatever its velocity is, that is affected by its acceleration
     angleFromYaxisToPendulumLineVelocity += angleFromYaxisToPendulumLineAcceleration;
-    //update the angle based on the velocity
+    //update the angle based on the velocity. Whatever actual angle is is affected by how fast it is going. 
     angleFromYaxisToPendulumLine += angleFromYaxisToPendulumLineVelocity;
-     
+    //In order to simulate slowing down, we multiply by a damper value. 
     angleFromYaxisToPendulumLineVelocity *= damperValue;
 
     //sin(angleFromYaxisToPendulumLine) = x/pendulumLen; and you must add from the origin because angle is from (0,0) origin
