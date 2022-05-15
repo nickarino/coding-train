d661b34f959d7968493038fc65546c7ef554b71e
Nick Skriloff <skriloffn@darden.virginia.edu>
2022-05-15 11:33:28 -0400

    get circle working

---
+++ CodingChallenges/CC_125_Fourier_Series/P5/Nick/sketch.js
@@ -0,0 +1,35 @@
+// Fourier Series
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/125-fourier-series.html
+// https://youtu.be/Mm2eYfj0SgA
+// https://editor.p5js.org/codingtrain/sketches/SJ02W1OgV
+
+let time = 0;
+let wave = [];
+
+let slider;
+
+function setup() {
+  createCanvas(600, 400);
+  slider = createSlider(1, 10, 5);
+}
+
+function draw() {
+  //set up the object properties
+  background(0);
+  translate(150, 200);
+  stroke(255);
+  noFill();
+
+
+  let radius = 100;
+  ellipse(0,0, radius *2);
+
+  let x = radius * cos(time);
+  let y = radius * sin(time);
+  fill(255);
+  ellipse(x ,y ,8);
+
+  time = time + .01;
+
+}
