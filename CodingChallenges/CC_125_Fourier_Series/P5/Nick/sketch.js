7c5e7648c968751c56f0489af385631abf77c512
Nick Skriloff <nick@ourdecisions.com>
2022-06-30 05:42:08 -0400

    clean up and learn

--- CodingChallenges/CC_125_Fourier_Series/P5/Nick/sketch.js
+++ CodingChallenges/CC_125_Fourier_Series/P5/Nick/sketch.js
@@ -4,64 +4,67 @@
 // https://youtu.be/Mm2eYfj0SgA
 // https://editor.p5js.org/codingtrain/sketches/SJ02W1OgV
 
-let time = 0;
+let time = 0; //unit = number of times it goes through "draw" method: clock speed
 let wave = []; //array
-let timeWarpSpeed = 5;
-let canvasWidth = 600;
+let timeWarpSpeed = 5; //constant
+let canvasWidth = 600; //pixels
 let slider;
-let xTranslate = 150;
-let yTranslate = 200;
+let xTranslate = 150; //pixels
+let yTranslate = 200; //pixels
+let radius = 75; // in pixels
 
 
 function setup() {
   createCanvas(canvasWidth, 400);
   slider = createSlider(1, 10, 5);
+  //common to all objects and can happen ONCE --does not have to be set up again. 
+  stroke("white");
+
 }
 
 function draw() {
 
-//draw first circle
-beginShape();
-  //set up the object properties
-  background(0);
+  //draw first circle
+  beginShape();
+  //set up the object properties that need to happen each time
+  background("black");
   translate(xTranslate, yTranslate);
-  stroke("white");
   noFill();
-  let n = 1;
-  let multForCircle = 75;
-  radius =  multForCircle * (4 / (n * PI));
+  //Pi = C/d = C/(2*r) => d = C/PI => r = C/(2*PI)
+  //degree = PI/180
   ellipse(0, 0, radius * 2);
-endShape();
+  endShape();
 
-//The angle of the arc = central angle = time
-// A unit circle cos(theta) = adjacent/hypotenuse = x/1 = x
-// A unit circl sin(theta) = opposite/hypotenuse = y/1 = y
-//normalize by radius: multiply. 
+  //The angle of the arc = central angle = time
+  // A unit circle cos(theta) = adjacent/hypotenuse = x/1 = x
+  // A unit circl sin(theta) = opposite/hypotenuse = y/1 = y
+  //normalize by radius: multiply. 
 
-let x = radius * cos(n * time);
-let y = radius * sin(n * time);
+  let x = radius * cos(time);
+  let y = radius * sin(time);
 
-wave.unshift(y); //in javascript wave.push appends while wave.unshift puts it at the beginnig
+  wave.unshift(y); //in javascript wave.push appends while wave.unshift puts it at the beginnig
 
-//draw the rotating radius with a dot at the end. 
-beginShape();
+  //draw the rotating radius with a dot at the end. 
+  beginShape();
   fill("white");
+  //tiny 8 pixel ball at point (x,y) to better accent the circle
   ellipse(x, y, 8);
-  line(0, 0, x, y)
-endShape();
+  line(0, 0, x, y); //vrom center of circle to (x,y)
+  endShape();
 
-//draw the sine wave
-beginShape();
+  //draw the line from x,y to sine wave (really just a set of vertexes) 
+  beginShape();
   let moveRight = 200;
-  translate(moveRight, 0);
-  line(x - moveRight, y, 0, wave[0]);
+  translate(moveRight, 0); //relative to the current translated shape
+  line(x - moveRight, y, 0, wave[0]); //wave was unshifted so wave[0] is always latest value
   noFill();
   for (let i = 0; i < wave.length; i++) {
     vertex(i, wave[i]);
   }
-  // do not need the array to belonger than 
+  // do not need the array to be longer than 400
   if (wave.length > 400) { wave.pop(); }
- endShape();
+  endShape();
 
   time = time + timeWarpSpeed * .01;
 
