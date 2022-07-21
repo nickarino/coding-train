f906d68c1f492fc13f5474f5bb4ae663b891508e
Nick Skriloff <skriloffn@darden.virginia.edu>
2022-07-21 18:03:25 -0400

    working on code

--- CodingChallenges/CC_125_Fourier_Series/P5/NickFourier/sketch.js
+++ CodingChallenges/CC_125_Fourier_Series/P5/NickFourier/sketch.js
@@ -23,17 +23,27 @@ function draw() {
     let prevx = x;
     let prevy = y;
 
-    // let n = i * 2 + 1;
-    // let radius = 75 * (4 / (n * PI));
-    // x += radius * cos(n * time);
-    // y += radius * sin(n * time);
+    //https://youtu.be/MY4luNgGfms?t=1481
+    let freq = fourierY.freq;  //time it takes to do one rotation around
+    let radius = fourierY.amp; //radius is the amplitude
+    let phase = fourierY.phase; //offset from origin
+    x += radius * cos(freq * time + phase);
+    y += radius * sin(freq * time + phase);
 
-    // stroke(255, 100);
-    // noFill();
-    // ellipse(prevx, prevy, radius * 2);
+    stroke(255, 100);
 
-    // stroke(255);
-    // line(prevx, prevy, x, y);
+    beginShape();
+    noFill();
+    for(let i=0;i<wave.length;i++){
+      vertex(i, wave[i]);
+    }
+    endShape();
+    //https://youtu.be/MY4luNgGfms?t=1583
+
+
+
+    stroke(255);
+    line(prevx, prevy, x, y);
  
   }
   wave.unshift(y);
